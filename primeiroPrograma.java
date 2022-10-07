3f4f4872d860a6c7b3c165d71a488617f7073c78
Getonio Castro <102559370+GetonioCastro@users.noreply.github.com>
2022-10-07 12:06:58 -0300

    Teste 02 Getonio 07 10 2022

---
+++ primeiroPrograma.java
@@ -0,0 +1,6 @@
+class MeuPrograma{
+    public static void main(String[] args){
+        System.out.printf("Meu primeiro JAVA da BOLSA\nSegunda Linha");
+        System.out.println("\nGetonio 3");
+    }
+}
